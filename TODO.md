d16c6a18f0679166a5273fa170ab4c928e767588
Sonia Hashim <soniapriyahashim@gmail.com>
2021-01-19 09:38:21 -0800

    experimenting with min functionality for deploying app?

---
+++ TODO.md
@@ -0,0 +1,6 @@
+#Features
+
+* Create new svg
+* Save svg
+* Add backend
+* Show svg files
--- ames.js
+++ ames.js
@@ -1,6 +1,10 @@
 console.log("I'm KING OF THE WORLD!")
 
-var draw = SVG().addTo('#canvas').size(400, 400)
+var draw = SVG().addTo('#canvas').size(1000, 1000)
 // var rect = draw.rect(100, 100).attr({ fill: '#f06' })
 
-let makeSquare = () => draw.rect(100,100).attr({fill: '#f06'}).draggable()
+let makeSquare = () => {
+	var r = draw.rect(100,100).attr({fill: '#f06'})
+	r.animate(2000, 1000, 'now').attr({ fill: '#fe3' })
+	r.draggable()
+}
